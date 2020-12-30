0984fdcaae22dc72f229dc044f9a13887afea144
nikitashrestha <nik.stha81@gmail.com>
2020-12-30 18:48:18 +0545

    main: Use conversion util to read data file

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from utils.files import read_file_S3
+from utils.conversion import convert_byte_to_df
 
 def main():
   data = read_file_S3('diamond-price-prediction','training_dataset/diamonds.csv')
-  print(data)
 
+  df=convert_byte_to_df(data)
+  print(df.isnull()==True)
+  
 
 if __name__=='__main__':
   main()
